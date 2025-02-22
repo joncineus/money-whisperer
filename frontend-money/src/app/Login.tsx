c14eb682f7cc5d3bb0b18f58c769f2dbeb11b7a5
Jonathan Alcineus <95049403+joncineus@users.noreply.github.com>
2025-02-22 11:55:16 -0500

    Added Login screen

---
+++ frontend-money/src/app/Login.tsx
@@ -0,0 +1,11 @@
+
+export default function Login() {
+    return (
+      <>
+        <h1>Welcome to MoneyWhisperer! Sign in if you dare</h1>
+        <input type="text"></input>
+        <input type="password"></input>
+      </>
+    );
+  }
+  
